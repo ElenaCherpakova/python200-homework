8e7100430ba011b13af17cc2c6fba53c3196db22
ElenaCherpakova <elena.cherpakova@gmail.com>
2026-09-08 18:07:49 -0400

    update path

--- assignments_01/project_01.py
+++ assignments_01/project_01.py
@@ -15,8 +15,7 @@ data_dir = "happiness_project"
 years = [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024]
 file_name = 'world_happiness'
 output_path = 'assignments_01/outputs/merged_happiness.csv'
-visual_output_path = 'outputs'
-
+visual_output_path = 'assignments_01/outputs'
 
 # Task 1: Load Multiple Years of Data
 @task(task_run_name="Load Data", retries=3, retry_delay_seconds=2)
