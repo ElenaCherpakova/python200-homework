9bc0d73c86477a5b74110f9ca53db2fca3c30311
ElenaCherpakova <elena.cherpakova@gmail.com>
2026-09-08 18:39:37 -0400

    remove space

--- assignments_03/warmup_03.py
+++ assignments_03/warmup_03.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
 from sklearn.datasets import load_iris, load_digits
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.preprocessing import StandardScaler
